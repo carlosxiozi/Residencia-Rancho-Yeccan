0dd62b6209fc1e4dc106142e3f37ca2049d014dc
carlosxiozi <carlosxiozi@gmail.com>
2022-01-10 13:35:29 -0600

    toros

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -12,7 +12,7 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
     <link rel = "stylesheet" href="{{asset('static/js/libss/mdtoast.min.css')}}">
-    <title>Document</title>
+    <title>Animales</title>
 </head>
 <body>
     <header class="header">
